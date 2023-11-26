61b8a72b9f6f3f579bca53f8dff56afd200c1278
root <root@debian11hostname.debian11domainname>
2023-11-26 10:38:59 +0200

    refactored demo apify-ipstack-openweathermap

--- demos/demo_linux_apify-ipstack-openweathermap.sh
+++ demos/demo_linux_apify-ipstack-openweathermap.sh
@@ -1,10 +1,16 @@
 myip=$(curl -X GET https://api.ipify.org)
 echo "$myip"
-mylat=$(curl -X GET http://api.ipstack.com/"$myip"?access_key=$APIKEY_IPSTACK | jq '.latitude')
+
+read mylat mylon mycity < <(echo $(curl -X GET http://api.ipstack.com/"$myip"?access_key=$APIKEY_IPSTACK | jq -r '.latitude, .longitude, .city'))
 echo "$mylat"
-mylon=$(curl -X GET http://api.ipstack.com/"$myip"?access_key=$APIKEY_IPSTACK | jq '.longitude')
 echo "$mylon"
-mycity=$(curl -X GET http://api.ipstack.com/"$myip"?access_key=$APIKEY_IPSTACK | jq '.city' | tr -d '"')
 echo "$mycity"
-mytemp=$(curl --location "http://api.openweathermap.org/data/2.5/weather?lat=$mylat&lon=$mylon&appid=$APIKEY_OPENWEATHERMAP&units=metric" | jq '.main.temp')
-echo -e "\033[0;32mTemperature in $mycity is $mytemp°C.\033[0m"
+
+while true; do
+echo "Press T to get current temperature for your IP location:"
+read -rsn1 input
+if [ "$input" = "t" ]; then
+    mytemp=$(curl --location "http://api.openweathermap.org/data/2.5/weather?lat=$mylat&lon=$mylon&appid=$APIKEY_OPENWEATHERMAP&units=metric" | jq '.main.temp')
+    echo -e "\033[0;32mTemperature in $mycity is $mytemp°C.\033[0m"
+fi
+done
